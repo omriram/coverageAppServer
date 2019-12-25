c6f09b4b4af1c9c216bafa769b22bc07b32a1da9
Omri Ram <omriram08@gmail.com>
2019-12-25 23:34:59 +0200

    changing back to local host

--- server.js
+++ server.js
@@ -4,19 +4,18 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const sourceCoverage = require("./controllers/source-coverage");
 
-/*     host: "127.0.0.1",
-    port: "3002",
-    user: "postgres",
-    password: "omri1990",
-    database: "coverage" */
-
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 const db = knex({
   client: "pg",
-  connection: process.env.DATABASE_URL,
-  ssl: true
+  connection: {
+    host: "127.0.0.1",
+    port: "3002",
+    user: "postgres",
+    password: "omri1990",
+    database: "coverage"
+  }
 });
 
 app.get("/", (req, res) => sourceCoverage.getAllCoverageRecords(req, res, db));
@@ -31,6 +30,6 @@ app.post("/deleteRecord", (req, res) =>
 app.get("/checkEmailDomain", (req, res) => {
   sourceCoverage.checkExistingDomainEmail(req, res, db);
 });
-const PORT = process.env.PORT || 3000;
+const PORT = /*  process.env.PORT ||  */ 3001;
 
 app.listen(PORT, () => console.log("server listen..."));
