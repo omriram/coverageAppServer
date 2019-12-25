382bbb3641081cadf27cf94745beb9ce5ebb79f4
Omri Ram <omriram08@gmail.com>
2019-12-25 21:36:15 +0200

    sdfsf

--- server.js
+++ server.js
@@ -4,18 +4,19 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const sourceCoverage = require("./controllers/source-coverage");
 
+/*     host: "127.0.0.1",
+    port: "3002",
+    user: "postgres",
+    password: "omri1990",
+    database: "coverage" */
+
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 const db = knex({
   client: "pg",
-  connection: {
-    host: "127.0.0.1",
-    port: "3002",
-    user: "postgres",
-    password: "omri1990",
-    database: "coverage"
-  }
+  connection: process.end.DATABASE_URL,
+  ssl: true
 });
 
 app.get("/", (req, res) => sourceCoverage.getAllCoverageRecords(req, res, db));
